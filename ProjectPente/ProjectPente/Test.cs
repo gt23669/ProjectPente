c5fbf1f6bd459ed8ce6d10f935d473d03f32a438
gt23669 <33846502+gt23669@users.noreply.github.com>
2018-10-11 14:49:47 -0600

    Updated "test" class to include rules and HTP

--- ProjectPente/ProjectPente/Test.cs
+++ ProjectPente/ProjectPente/Test.cs
@@ -8,6 +8,26 @@ namespace ProjectPente
 {
     class Test
     {
-        //repo test
+        /*
+         objective:
+            place 5 or more objects in a line. Horizontal, vertical, diagonal.
+            capture 5 or more pairs of objects
+        HTP:    
+            start with empty board
+            player 1(black) places objects on center spot
+            players then take turns placing objects
+            objects are placed on line intersections including the edge of the board
+            objects can not move around once placed aside from capture
+            continue adding objects to the board until one person wins
+        Capture:
+            Whenever your opponent has two objects (and only two) which are adjacent, they are up for capture. bracket their objects with your own to capture those two objects. Captured objects are removed from the board
+        WTG:
+            One player captures 5 pairs
+            one player lines 5 objects in a row
+            opponent has no "last chance"
+        TOURNAMENT RULES:
+            first player's first move must be on the center point. the first player's second move is restricted to playing three or more intersections away from the center of the board. All other rules of standard Pente apply.
+         */
+
     }
 }
