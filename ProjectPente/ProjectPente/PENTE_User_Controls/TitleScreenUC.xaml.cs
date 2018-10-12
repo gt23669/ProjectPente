8aae4588e95ba9c1b772c33630c8020aa7fc0358
gt23669 <33846502+gt23669@users.noreply.github.com>
2018-10-12 13:46:00 -0600

    GUI User Control Pages
    (work in progress)

---
+++ ProjectPente/ProjectPente/PENTE_User_Controls/TitleScreenUC.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectPente.PENTE_User_Controls
+{
+    /// <summary>
+    /// Interaction logic for TitleScreen.xaml
+    /// </summary>
+    public partial class TitleScreen : UserControl
+    {
+        MainWindow window;
+
+        public TitleScreen()
+        {
+            InitializeComponent();
+        }
+   
+        private MainWindow getWindow()
+        {
+            return Application.Current.MainWindow as MainWindow;
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
